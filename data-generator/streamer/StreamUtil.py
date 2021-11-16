463b3f6bfffb59b1a66bf5fdf41375ad0cf013fc
Vipin Kumar <vipin.kumar@walmartlabs.com>
2021-11-16 09:25:40 +0530

    insert batch

--- data-generator/streamer/StreamUtil.py
+++ data-generator/streamer/StreamUtil.py
@@ -3,6 +3,8 @@ from os import listdir
 from os.path import isfile, join
 import pandas as pd
 from cassandra.cluster import Cluster
+from cassandra.query import BatchStatement, ConsistencyLevel, SimpleStatement
+
 from streamer.BlockingQueue import BlockingQueue
 from streamer.pojo import YoutubeStart
 from datetime import datetime
@@ -17,23 +19,39 @@ class StreamUtil:
         self.session = self.cluster.connect('youtube_videos')
         self._queue = BlockingQueue(200)
 
+    @staticmethod
+    def convert_bool(value):
+        if value == "TRUE":
+            return True
+        return False
+
     def load_data(self):
         logging.info('draining queue ')
         item = self._queue.dequeue()
-        self.session.execute(
-            """
-            INSERT INTO users (video_id, trending_date, title, channel_title, category_id, publish_time, tags, views, 
-            likes, dislikes, comment_count, thumbnail_link, comments_disabled, ratings_disabled, 
-            video_error_or_removed, description, country)
-            VALUES (%s, %s, %s, %s, %s, %s, %s , %d, %d, %d, %d ,%s, %s, %s, %s, %s ,%s )
-            """,
-            (item.video_id, datetime.strptime(item.trending_date, '%y.%d.%m'), item.title, item.channel_title,
-             item.category_id, datetime.strptime(item.publish_time, '%Y-%m-%dT%H:%M:%S.%fZ'),
-             item.tags, int(item.views),
-             int(item.likes), int(item.dislikes), int(item.comment_count), item.thumbnail_link, item.comments_disabled,
-             item.ratings_disabled,
-             item.video_error_or_removed, item.description, item.country)
-        )
+
+        insert_data = self.session.prepare(
+            'INSERT INTO youtube_videos.stats_data (video_id, trending_date, title, channel_title, '
+            'category_id, publish_time, tags, views, likes, dislikes, comment_count, thumbnail_link, '
+            'comments_disabled, ratings_disabled,video_error_or_removed, description, country) '
+            'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? , ?) ')
+        batch = BatchStatement(consistency_level=ConsistencyLevel.ANY)
+        try:
+            batch.add(insert_data,
+                      (str(item.video_id), datetime.strptime(item.trending_date, '%y.%d.%m'), str(item.title),
+                       str(item.channel_title),
+                       str(item.category_id), datetime.strptime(item.publish_time, '%Y-%m-%dT%H:%M:%S.%fZ'),
+                       str(item.tags), int(item.views),
+                       int(item.likes), int(item.dislikes), int(item.comment_count), str(item.thumbnail_link),
+                       StreamUtil.convert_bool(item.comments_disabled),
+                       StreamUtil.convert_bool(item.ratings_disabled),
+                       StreamUtil.convert_bool(item.video_error_or_removed),
+                       str(item.description), str(item.country)))
+            logging.info('Data Inserted into the table')
+            print(insert_data.query_string)
+
+        except Exception as e:
+            logging.info('The cassandra error: {}'.format(e))
+        self.session.execute(batch)
         print(item)
         logging.info("item drained")
 
